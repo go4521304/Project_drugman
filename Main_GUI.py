d0faa5a557412768846168a328ef601f2aa39cff
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-05-16 22:34:53 +0900

    Update Main_GUI.py

--- Main_GUI.py
+++ Main_GUI.py
@@ -3,9 +3,19 @@
 from tkinter import *
 
 class Main_GUI:
+    def Draw_P(self): # Parmachy
+        pass
+    def Draw_M(self): # Medicine
+        pass
+
     def __init__(self):
         window = Tk()
         window.title("약 좀 주세요 ~약 먹을 시간~")
+
+        self.canvas = Canvas(window,bg = 'white', width = 1200, height = 900)
+        self.canvas.pack()
+
+       
 
         window.mainloop()
 
