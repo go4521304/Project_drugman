70db71ce9c98d2422cf32896ad1ad981f2dfa0ee
Kyulestial(MSI) <clsrn863@naver.com>
2021-05-20 15:01:32 +0900

    1

---
+++ medicine.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import font
+import tkinter.messagebox
+
+class Medicine():
+    def __init__(self):
+        Md = Tk()
+        #Md.geometry("크기입력")
+
+        Md.mainloop()
+
+Medicine()
