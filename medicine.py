16d661d190f3b2fc8c53014889976f134a9f21c3
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-05-21 21:42:27 +0900

    GOGO

--- medicine.py
+++ medicine.py
@@ -2,11 +2,72 @@ from tkinter import *
 from tkinter import font
 import tkinter.messagebox
 
-class Medicine():
-    def __init__(self):
-        Md = Tk()
-        #Md.geometry("크기입력")
+import urllib
+import http.client
+conn = http.client.HTTPConnection("apis.data.go.kr")
+serviceKey = urllib.parse.quote("6LhIfl4AUMeRlvkH0mimycui3rFJsijVvZWIHR4mt5/symnzRkA+Zi4WZJ8ietyQ/LnNBAKhnSirL3lRX7WllA==")
+Md = Tk()
+#class Medicine():
+SearchListBox = (0)
 
-        Md.mainloop()
+def Main():
+    Md.title("Medicine")
+    Md.geometry("600x800")
+    conn.request("GET", "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService?serviceKey=" + serviceKey)
+    req = conn.getresponse()
+    print(req.status, req.reason)
+    print(req.read().decode('utf-8'))
+    Md.create_rectangle()
 
-Medicine()
+def Search_efcyQesitm():
+    global SearchListBox
+    ListBoxScrollbar = Scrollbar(Md)
+    ListBoxScrollbar.pack()
+    ListBoxScrollbar.place(x=130, y=50)
+
+    TempFont = font.Font(Md, size=15, weight='bold', family='Consolas')
+    SearchListBox = Listbox(Md, font=TempFont, activestyle='none',
+                            width=10, height=1,
+                            yscrollcommand=ListBoxScrollbar.set)
+
+    SearchListBox.insert(0, "증상")
+    SearchListBox.insert(1, "약 이름")
+    SearchListBox.insert(2, "제약사")
+    SearchListBox.pack()
+    SearchListBox.place(x=10, y=60)
+
+    ListBoxScrollbar.config(command=SearchListBox.yview)
+
+def InitInputLabel():
+    global InputLabel
+    TempFont = font.Font(Md, size=15, weight='bold', family = 'Consolas')
+    InputLabel = Entry(Md, font = TempFont, width = 26, relief = 'ridge')
+    InputLabel.pack()
+    InputLabel.place(x=160, y=60)
+
+def InitSearchButton():
+    TempFont = font.Font(Md, size=12, weight='bold', family = 'Consolas')
+    SearchButton = Button(Md, font = TempFont, text="검색",  command=SearchButtonAction)
+    SearchButton.pack()
+    SearchButton.place(x=480, y=55)
+
+def SearchButtonAction():
+    global SearchListBox
+
+    iSearchIndex = SearchListBox.curselection()[0]
+
+    if iSearchIndex == 0:
+        print("증상")
+    elif iSearchIndex == 1:
+        print("약 이름")
+    elif iSearchIndex == 2:
+        print("제약사")
+
+
+
+
+Main()
+Search_efcyQesitm()
+InitInputLabel()
+InitSearchButton()
+Md.mainloop()
