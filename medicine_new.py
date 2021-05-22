562977bc595a28930b5bd193db76fa201ba783a1
Kclient <go4521304@gmail.com>
2021-05-22 18:14:00 +0900

    데이터 프레임 삭제

--- medicine_new.py
+++ medicine_new.py
@@ -1,5 +1,4 @@
 from typing import Sized
-import pandas as pd
 import urllib
 import http.client
 conn = http.client.HTTPConnection("apis.data.go.kr")
@@ -7,18 +6,17 @@ conn = http.client.HTTPConnection("apis.data.go.kr")
 class Medicine:
     SERVICE_KEY = urllib.parse.quote("6LhIfl4AUMeRlvkH0mimycui3rFJsijVvZWIHR4mt5/symnzRkA+Zi4WZJ8ietyQ/LnNBAKhnSirL3lRX7WllA==")
 
-    medicine = None
-    index = 0
+    medicine = []
 
     TYPE = {'0': '&entpName=', '1': '&itemName=', '2': '&efcyQesitm='}
+    COLUMNS = ['업체명', '제품명', 'ID', '효능', '사용법', '주의사항 경고', '주의사항', '상호작용', '부작용', '보관법', '낱알 이미지']
 
     def __init__(self):
         pass
 
     def request(self, type = '0', option = '유한양행'):
-        # 데이터프레임 생성
-        self.medicine = pd.DataFrame(columns=['업체명', '제품명', 'ID', '효능', '사용법', '주의사항 경고', '주의사항', '상호작용', '부작용', '보관법', '낱알 이미지'])
-        self.index = 0
+        # 리스트로 생성
+        self.medicine.clear()
 
         # 옵션에 따라 쿼리 생성
         find_query = self.TYPE[type] + urllib.parse.quote(option)
@@ -45,10 +43,10 @@ class Medicine:
         if resultCode.text == '00':
             item = tree.iter('item')
             for i in item:
-                self.medicine.loc[self.index] = [i.find('entpName').text, i.find('itemName').text, 
-                i.find('itemSeq').text, i.find('efcyQesitm').text, i.find('useMethodQesitm').text, i.find('atpnWarnQesitm').text, i.find('atpnQesitm').text,
-                i.find('intrcQesitm').text, i.find('seQesitm').text, i.find('depositMethodQesitm').text, i.find('itemImage').text]
-                self.index+=1
+                self.medicine.append({self.COLUMNS[0]:i.find('entpName').text, self.COLUMNS[1]:i.find('itemName').text, 
+                self.COLUMNS[2]:i.find('itemSeq').text, self.COLUMNS[3]:i.find('efcyQesitm').text, self.COLUMNS[4]:i.find('useMethodQesitm').text,
+                self.COLUMNS[5]: i.find('atpnWarnQesitm').text, self.COLUMNS[6]:i.find('atpnQesitm').text, self.COLUMNS[7]:i.find('intrcQesitm').text, 
+                self.COLUMNS[8]:i.find('seQesitm').text, self.COLUMNS[9]:i.find('depositMethodQesitm').text, self.COLUMNS[10]:i.find('itemImage').text})
         else:
             print('Load Fail / Code : ' + resultCode.text)
         
@@ -57,3 +55,4 @@ class Medicine:
 test = Medicine()
 test.request()
 print(test.medicine)
+print(len(test.medicine))
