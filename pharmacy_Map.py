883b714acb4ddd151103f2104a41dfaca46c3811
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-05-25 17:46:17 +0900

    a

--- pharmacy_Map.py
+++ pharmacy_Map.py
@@ -3,24 +3,34 @@ import folium
 import webbrowser
 import pharmacy as ph
 
-def Pressed():
-    # 위도 경도 지정
-    test = ph.Pharmacy()
-    test.request()
 
-    print(test.pharmacy['LON'][1])
-    print(test.pharmacy['LAT'][1])
-    map_osm = folium.Map(location=[test.pharmacy['LON'][1], test.pharmacy['LAT'][1]], zoom_start=13)
-    # 마커 지정
-    folium.Marker([test.pharmacy['LON'][1], test.pharmacy['LAT'][1]],
-                  popup='한국산업기술대').add_to(map_osm)
-    map_osm.save('osm.html')
-    webbrowser.open_new('osm.html')
+class MAP:
+    def __init__(self):
+        self.Main()
 
-    #print(test.pharmacy['주소'][1])
+    def Main(self):
+        self.window = Tk()
+        canvas = Canvas()
+        canvas.
+        Button(self.window, text='folium 지도', command=self.Pressed).pack()
+        self.window.mainloop()
 
-    # html 파일로 저장
+    def Pressed(self):
+        # 위도 경도 지정
+        test = ph.Pharmacy()
+        test.request()
 
-window= Tk()
-Button(window, text='folium 지도', command=Pressed).pack()
-window.mainloop()
+        print(test.pharmacy['LON'][1])
+        print(test.pharmacy['LAT'][1])
+        map_osm = folium.Map(location=[test.pharmacy['LON'][1], test.pharmacy['LAT'][1]], zoom_start=13)
+        # 마커 지정
+        folium.Marker([test.pharmacy['LON'][1], test.pharmacy['LAT'][1]],
+                      popup='한국산업기술대').add_to(map_osm)
+        # html 파일로 저장
+        map_osm.save('osm.html')
+        webbrowser.open_new('osm.html')
+
+        #print(test.pharmacy['주소'][1])
+
+MAP()
+
