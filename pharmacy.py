f2727c8bf5a106f36921e65b5505c23cb0d806f4
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-06-02 21:13:50 +0900

    그래프 V0.8

--- pharmacy.py
+++ pharmacy.py
@@ -82,32 +82,26 @@ class Pharm:
                                         command=self.SearchButtonAction,
                                         width=7)
         searchButton.place(x=610, y=18)     # 좌표
-        self.Graph(self.frameTop)
 
     def Graph(self,window):
         tk.Label(window, text='Bar Chart')
-        data = [21, 20, 19, 16, 14, 13, 11, 9, 4, 3]
-        c_width = 400
-        c_height = 350
+        data = [21, 20, 19, 16, 14, 13, 11, 9, 4, 3, 0, 1, 0, 14, 14]
+        c_width = 870
+        c_height = 300
         c = tk.Canvas(window, width=c_width, height=c_height, bg='white')
-        c.place(x=0,y=0)
+        c.place(x=0,y=500)
 
-        # experiment with the variables below size to fit your needs
+        Key = self.strAdd1.get()
+        Data = self.pharm.request(self.strAdd1.get(), self.strAdd2.get())
+        print(Key)
+        print(Data)
+        print(self.searchAdd2.get())
+        Area = self.address_data[self.strAdd1.get()] #시/군/구 리스트
+        Num = len(self.address_data[Key]) #시/군/구 갯수
+        for i in range(Num):
+            c.create_rectangle((840/Num) * i, 200, 20+(840/Num) * i, 300, fill="light green")
+            c.create_text(17+(840/Num) * i, 300, anchor=tk.SW, text=Area[i],font = ('furisa',10),angle=90)
 
-        y_stretch = 15
-        y_gap = 20
-        x_stretch = 10
-        x_width = 20
-        x_gap = 20
-        for x, y in enumerate(data):
-            # calculate reactangle coordinates
-            x0 = x * x_stretch + x * x_width + x_gap
-            y0 = c_height - (y * y_stretch + y_gap)
-            x1 = x * x_stretch + x * x_width + x_width + x_gap
-            y1 = c_height - y_gap
-            # Here we draw the bar
-            c.create_rectangle(x0, y0, x1, y1, fill="red")
-            c.create_text(x0 + 2, y0, anchor=tk.SW, text=str(y))
 
     def showMap(self, frame):
         sys.excepthook = cef.ExceptHook
@@ -143,3 +137,4 @@ class Pharm:
     def selAdd1(self, event):
         self.searchAdd2.set("시/군/구")
         self.searchAdd2['value'] = self.address_data[self.strAdd1.get()]
+        self.Graph(self.frameTop)
