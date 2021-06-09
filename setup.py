6b34fea48ee19c7c3a018f1212524721422d86c5
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-06-09 23:22:28 +0900

    배포판 만들기

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup, Extension
+
+setup(name='Drug',
+      version='1.0',
+      py_modules=['Main_GUI','medicine','medicine_conn','pharmacy','pharmacy_conn','Search','telegram','telegram_conn','예제'])
