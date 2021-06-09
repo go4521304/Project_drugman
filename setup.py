c6ad6f2bcc590559b33f59c998187b8d349a0411
Mingo <34563275+Seulkyu42@users.noreply.github.com>
2021-06-09 23:35:48 +0900

    고고

--- setup.py
+++ setup.py
@@ -2,4 +2,8 @@ from distutils.core import setup, Extension
 
 setup(name='Drug',
       version='1.0',
-      py_modules=['Main_GUI','medicine','medicine_conn','pharmacy','pharmacy_conn','Search','telegram','telegram_conn','예제'])
+      py_modules=['Main_GUI','medicine','medicine_conn','pharmacy','pharmacy_conn','Search','telegram','telegram_conn'],
+      data_files=[('resource',['resource\\address_list.json','resource\\logo-gmail.png','resource\\medicine.png','resource\\medicine2.png',
+                               'resource\\pharmacy.png','resource\\pharmacy2.png'
+                               ,'resource\\search.png','resource\\search2.png','resource\\text.png'],)]
+      )
